a4a6b53b8f37868cbacc1786861a566c3a10570b
Alex Haslam <alex.haslam@carbonre.com>
2025-03-25 23:01:06 +0000

    Add option to backtest

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ import numpy as np
 import pandas as pd
 import datetime as dt
 import plotly.graph_objects as go
+import yfinance as yf
 
 pd.options.plotting.backend = "plotly"
 
@@ -12,52 +13,122 @@ pd.options.plotting.backend = "plotly"
 class Asset:
     returns: float
     volatility: float
+    ticker: str | None = None
+
+
+def run_simulation(
+    starting_amount: float,
+    monthly_contributions: pd.Series,
+    growth: pd.DataFrame,
+    inflation: float,
+) -> tuple[pd.DataFrame, pd.Series]:
+    # Compute portfolio values
+    portfolio = pd.DataFrame(index=growth.index, columns=growth.columns)
+    portfolio.iloc[0, :] = starting_amount
+    for i in range(1, growth.shape[0]):
+        portfolio.iloc[i, :] = (
+            (portfolio.iloc[i - 1, :]) * growth.iloc[i, :]
+            + monthly_contributions.iloc[i - 1]
+        )
+
+    # Adjust for inflation
+    inflation_adjustment = (1 + inflation / 12) ** np.arange(growth.shape[0])
+    years = (growth.index[-1] - growth.index[0]).total_seconds() / (365.25 * 24 * 3600)
+    aer = growth.divide(inflation_adjustment, axis=0).prod().pow(1 / years) - 1
+    return portfolio.divide(inflation_adjustment, axis=0), aer
+
+
+def _sample_volatile_growth(
+    assets: dict[str, Asset],
+    allocation: pd.DataFrame,
+    num_simulations: int,
+    seed: int = 42,
+) -> pd.DataFrame:
+    np.random.seed(seed)
+    total_growth = pd.DataFrame(
+        index=allocation.index, columns=range(num_simulations), data=0.0
+    )
+    for name, asset in assets.items():
+        monthly_returns = np.random.normal(
+            asset.returns / 12,
+            asset.volatility / np.sqrt(12),
+            (allocation.shape[0], num_simulations),
+        )
+        growth = (1 + monthly_returns) * allocation[name].values[:, np.newaxis]
+        total_growth += growth
+    return total_growth
+
+
+def _fetch_historic_growth(
+    assets: dict[str, Asset], allocation: pd.DataFrame
+) -> pd.DataFrame:
+    total_growth = pd.Series(index=allocation.index, data=0.0)
+    for name, asset in assets.items():
+        assert asset.ticker, f"Missing ticker for asset {name}"
+        ticker_data = yf.download(
+            asset.ticker,
+            start=allocation.index[0] - dt.timedelta(days=31),
+            end=allocation.index[-1],
+            interval="1mo",
+        )
+        monthly_returns = (
+            ticker_data[("Close", asset.ticker)]
+            .pct_change()
+            .reindex(allocation.index, method="ffill")
+        )
+        total_growth += (1 + monthly_returns) * allocation[name].values
+    return total_growth.to_frame()
 
 
 @st.cache_data
-def simulator(
+def monte_carlo(
     starting_amount: float,
     monthly_contributions: pd.Series,
     assets: dict[str, Asset],
     allocation: pd.DataFrame,
     inflation: float,
     fees: float,
-    years: int,
+    start_date: dt.date,
+    end_date: dt.date,
     num_simulations: int = 1,
     seed: int = 42,
 ) -> tuple[pd.DataFrame, pd.Series]:
-    months = years * 12
-    index = pd.date_range(dt.date.today(), periods=months, freq="ME").date
-
-    monthly_contributions = monthly_contributions.reindex(index, method="ffill")
-
-    total_growth = pd.DataFrame(index=index, columns=range(num_simulations), data=0.0)
-    np.random.seed(seed)
-    allocation = allocation.reindex(index, method="ffill")
-    for name, asset in assets.items():
-        monthly_returns = np.random.normal(
-            asset.returns / 12,
-            asset.volatility / np.sqrt(12),
-            (months, num_simulations),
+    index = pd.date_range(start_date, end_date, freq="ME").date
+    return run_simulation(
+        starting_amount=starting_amount,
+        monthly_contributions=monthly_contributions.reindex(index, method="ffill"),
+        growth=_sample_volatile_growth(
+            assets=assets,
+            allocation=allocation.reindex(index, method="ffill"),
+            num_simulations=num_simulations,
+            seed=seed,
         )
-        growth = (1 + monthly_returns) * allocation[name].values[:, np.newaxis]
-        total_growth += growth
+        - fees / 12,
+        inflation=inflation,
+    )
 
-    total_growth -= fees / 12
 
-    # Compute portfolio values
-    portfolio = pd.DataFrame(index=index, columns=range(num_simulations))
-    portfolio.iloc[0, :] = starting_amount
-    for i in range(1, months):
-        portfolio.iloc[i, :] = (
-            (portfolio.iloc[i - 1, :]) * total_growth.iloc[i, :]
-            + monthly_contributions.iloc[i - 1]
+@st.cache_data
+def backtest(
+    starting_amount: float,
+    monthly_contributions: pd.Series,
+    assets: dict[str, Asset],
+    allocation: pd.DataFrame,
+    inflation: float,
+    fees: float,
+    start_date: dt.date,
+    end_date: dt.date,
+) -> tuple[pd.DataFrame, pd.Series]:
+    index = pd.date_range(start_date, end_date, freq="ME").date
+    return run_simulation(
+        starting_amount=starting_amount,
+        monthly_contributions=monthly_contributions.reindex(index, method="ffill"),
+        growth=_fetch_historic_growth(
+            assets=assets, allocation=allocation.reindex(index, method="ffill")
         )
-
-    # Adjust for inflation
-    inflation_adjustment = (1 + inflation / 12) ** np.arange(months)
-    aer = total_growth.divide(inflation_adjustment, axis=0).prod().pow(1 / years) - 1
-    return portfolio.divide(inflation_adjustment, axis=0), aer
+        - fees / 12,
+        inflation=inflation,
+    )
 
 
 # Streamlit UI
@@ -68,8 +139,8 @@ st.title("Portfolio Growth Simulator")
 
 if "default_assets" not in st.session_state:
     st.session_state["default_assets"] = {
-        "Equities": Asset(returns=5.0 / 100, volatility=25 / 100),
-        "Bonds": Asset(returns=2.0 / 100, volatility=0.0 / 100),
+        "Equities": Asset(returns=5.0 / 100, volatility=25 / 100, ticker="VWRP.L"),
+        "Bonds": Asset(returns=2.0 / 100, volatility=0.0 / 100, ticker="VAGS.L"),
     }
 
 if "default_allocation" not in st.session_state:
@@ -78,7 +149,7 @@ if "default_allocation" not in st.session_state:
             k: [1.0 / len(st.session_state["default_assets"])]
             for k in st.session_state["default_assets"]
         },
-        index=[dt.date.today()],
+        index=[dt.date.today() - dt.timedelta(days=3 * 365.25)],
     )
     default_allocation.index.name = "Date"
     st.session_state["default_allocation"] = default_allocation
@@ -94,12 +165,23 @@ def _get_name(assets: dict[str, Asset]) -> str:
 
 
 with st.sidebar:
+    st.header("Simulation")
+    container = st.container()
+    end_date = st.date_input(
+        "End Date", max_value=dt.date.today(), value=dt.date.today()
+    )
+    with container:
+        start_date = st.date_input(
+            "Start Date",
+            value=dt.date.today() - dt.timedelta(days=3 * 365.25),
+            max_value=end_date,
+        )
     st.header("Funds")
     starting_amount = st.number_input("Starting amount (£)", value=10000, step=1000)
     contributions = st.data_editor(
         pd.DataFrame(
             {
-                "Date": [dt.date.today()],
+                "Date": [start_date],
                 "Monthly Contribution": [1000.0],
             }
         ),
@@ -148,6 +230,9 @@ with st.sidebar:
                             )
                             / 100
                         ),
+                        ticker=st.text_input(
+                            "Ticker", asset.ticker, key=f"{name}_ticker"
+                        ),
                     )
                     if len(st.session_state["default_assets"]) > 1:
                         if st.button(":material/delete:", key=f"{name}_delete"):
@@ -155,8 +240,10 @@ with st.sidebar:
 
     with tabs[1]:
         last_asset = list(selected_assets.keys())[-1]
+        default_allocation = st.session_state["default_allocation"]
+        default_allocation.index = default_allocation.index + (start_date -  default_allocation.index[0])
         allocation: pd.DataFrame = st.data_editor(
-            st.session_state["default_allocation"].reset_index(),
+            default_allocation.reset_index(),
             num_rows="dynamic",
             hide_index=True,
             column_config={
@@ -196,21 +283,24 @@ with st.sidebar:
             columns=renamed_assets
         )
         st.rerun()
+
     st.header("Settings")
     inflation = st.number_input("Inflation Rate (%)", value=0.0, step=0.1) / 100
     fees = st.number_input("Annual fees (%)", value=0.4, step=1.0) / 100
 
+mode = st.radio("Select mode", ["Monte-carlo", "Backtesting"], index=0, horizontal=True)
 
-cols = st.columns(2)
-with cols[0]:
-    years = st.number_input("Number of Years", value=38, step=1)
-with cols[1]:
-    num_simulations = st.number_input("Number of Simulations", value=10000, step=1)
-run_simulation = st.button("Run Simulation")
+columns = st.columns([0.15, 0.85])
+with columns[0]:
+    simulate = st.button("Run Simulation")
+with columns[1]:
+    if mode == "Monte-carlo":
+        with st.popover(":material/settings:"):
+            num_simulations = st.number_input("Number of Simulations", value=10000, step=1)
+
 st.divider()
 
-
-if run_simulation:
+if simulate:
     confidence = st.radio(
         "Select confidence interval",
         [0.9, 0.95, 0.99],
@@ -219,17 +309,32 @@ if run_simulation:
         horizontal=True,
     )
 
-    simulation_dfs, aer = simulator(
-        starting_amount=starting_amount,
-        monthly_contributions=contributions["Monthly Contribution"],
-        allocation=allocation,
-        assets=selected_assets,
-        inflation=inflation,
-        fees=fees,
-        years=years,
-        num_simulations=num_simulations,
-        seed=42,
-    )
+    if mode == "Monte-carlo":
+        simulation_dfs, aer = monte_carlo(
+            starting_amount=starting_amount,
+            monthly_contributions=contributions["Monthly Contribution"],
+            allocation=allocation,
+            assets=selected_assets,
+            inflation=inflation,
+            fees=fees,
+            start_date=start_date,
+            end_date=end_date,
+            num_simulations=num_simulations,
+            seed=42,
+        )
+    elif mode == "Backtesting":
+        simulation_dfs, aer = backtest(
+            starting_amount=starting_amount,
+            monthly_contributions=contributions["Monthly Contribution"],
+            allocation=allocation,
+            assets=selected_assets,
+            inflation=inflation,
+            fees=fees,
+            start_date=start_date,
+            end_date=end_date,
+        )
+    else:
+        raise ValueError(f"Unknown mode {mode}")
 
     aggregate_df = pd.DataFrame(
         {
@@ -288,7 +393,7 @@ if run_simulation:
     st.plotly_chart(
         simulation_dfs.iloc[-1]
         .hist(
-            nbins=int(max(num_simulations / 50, 10)),
+            nbins=int(max(simulation_dfs.shape[1] / 50, 10)),
             histnorm="percent",
             cumulative=st.toggle("Show cumulative distribution", False),
             title="Final portfolio value",
@@ -318,7 +423,7 @@ if run_simulation:
     )
 
     st.plotly_chart(
-        pd.DataFrame({"value": simulation_dfs.iloc[-1], "aer": 100*aer}).plot.scatter(
+        pd.DataFrame({"value": simulation_dfs.iloc[-1], "aer": 100 * aer}).plot.scatter(
             x="value",
             y="aer",
             title="Portfolio Value vs AER",
